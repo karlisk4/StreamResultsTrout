cfd4a46917e4f5dcb4bec478f3ffa39626141520
Kārlis Skirmanis <Karlis.Skirmanis@edu.rtu.lv>
2024-01-13 20:57:14 +0200

    corrected time display, split names for OBS display

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ drivers_json = resFile.json()
 i=1
 lenMax=0
 driver_info = []
+driver_stats = []
 for drivers in drivers_json['Result']:
         stI = str(i)
         driverName = drivers['DriverName']
@@ -20,17 +21,25 @@ for drivers in drivers_json['Result']:
         diffPos = qPos-i
         driverLine = "{stI}. {driverName} ".format(stI=stI, driverName=driverName)
         fstLapMs=int(drivers['BestLap'])
-        fstlap=timedelta(milliseconds=fstLapMs)
+        fstlap=str(timedelta(milliseconds=fstLapMs))
+        a = fstlap.replace("0:0","0")
+        b = a.replace("000","")
+        fstlap = b
         if len(driverLine) > lenMax:
                 lenMax = len(driverLine)
 
-        driver_info.append('{}{}{}'.format(driverLine.ljust(32), str(diffPos).ljust(10), fstlap)) #"{stI}. {driverName} ".format(stI=stI, driverName=driverName)
+        driver_info.append(driverLine) # 
+        driver_stats.append('{}{}'.format(str(diffPos).ljust(10), fstlap))
         i=i+1
 
 
 # for info in drivers_json['SessionConfig']:
 #         print(info)
 #print(driver_info)
-f = open("demofile3.txt", "w")
-for driver in driver_info:
-    f.write("%s\n" % driver)
+with open("names.txt", "w") as f:
+       for driver in driver_info:
+                f.write("%s\n" % driver)
+
+with open("result.txt", "w") as g:
+               for driver in driver_stats:
+                g.write("%s\n" % driver)
